2df100b2703185ec88ee705aa117c409c6e9a984
mu57f4 <mustafataha874@gmail.com>
2024-05-16 02:35:51 +0300

    Restricted applicants from access company form

--- company/views.py
+++ company/views.py
@@ -7,23 +7,27 @@ from users.models import User
 
 # update company
 def update_company(request):
-    company = Company.objects.get(user=request.user)
-    if request.method == 'POST':
-        form = UpdateCompanyForm(request.POST, instance=company)
-        if form.is_valid():
-            var = form.save(commit=False)
-            user = User.objects.get(id=request.user.id)
-            user.has_company = True
-            var.save()
-            user.save()
-            messages.success(request, 'Your company is now active, you can start creating job ads')
-            return redirect('dashboard')
+    if request.user.is_recruiter:
+        company = Company.objects.get(user=request.user)
+        if request.method == 'POST':
+            form = UpdateCompanyForm(request.POST, instance=company)
+            if form.is_valid():
+                var = form.save(commit=False)
+                user = User.objects.get(id=request.user.id)
+                user.has_company = True
+                var.save()
+                user.save()
+                messages.success(request, 'Your company is now active, you can start creating job ads')
+                return redirect('dashboard')
+            else:
+                messages.warning(request, 'Opps!, Something went wrong')
         else:
-            messages.warning(request, 'Opps!, Something went wrong')
+            form = UpdateCompanyForm(instance=company)
+            context = {'form': form}
+            return render(request, 'company/update_company.html', context)
     else:
-        form = UpdateCompanyForm(instance=company)
-        context = {'form': form}
-        return render(request, 'company/update_company.html', context)
+        messages.warning(request, 'Permission Denied')
+        return redirect('dashboard')
     
 
 def company_details(request, pk):
