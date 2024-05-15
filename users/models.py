f646c4df774e4fbf3b4c368c15ed91b6301cd4ba
mu57f4 <mustafataha874@gmail.com>
2024-05-16 01:12:26 +0300

    Added has_company

--- users/models.py
+++ users/models.py
@@ -5,3 +5,4 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_recruiter = models.BooleanField(default=False)
     is_applicant = models.BooleanField(default=False)
+    has_company = models.BooleanField(default=False)
