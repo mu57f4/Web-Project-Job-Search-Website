0b627c99cd841c268bf62a9c72ddfeb6c22b3a8c
mu57f4 <mustafataha874@gmail.com>
2024-05-15 15:54:22 +0300

    Checked If the user logged in

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 
 def dashboard(request):
+    username = request.user.username if request.user.is_authenticated else 'Guest'
     return render(
         request, 'dashboard/dashboard.html',
-        {'username': request.user.username}
+        {'username': username}
         )
