f696982a07130cc28815b6577be7d9b93389eac1
mu57f4 <mustafataha874@gmail.com>
2024-05-18 17:54:04 +0300

    fix company-details path

--- company/urls.py
+++ company/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('update-company/', views.update_company, name='update-company'),
-    path('company-details/<int:pk>/', views.update_company, name='company-details'),
+    path('company-details/<int:pk>/', views.company_details, name='company-details'),
 ]
