7737fcc94f67820a202656d8d5898bb44b7057ce
mu57f4 <mustafataha874@gmail.com>
2024-05-18 18:33:27 +0300

    Initiate resume model

---
+++ resume/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from users.models import User
+
+
+class Resume(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    job_title = models.CharField(max_length=100, null=True, blank=True)
+    # todo:add cv
