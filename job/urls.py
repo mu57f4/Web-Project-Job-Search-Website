5223b87bbd6a8a7bcf1abdd1b75c5945d2fa1067
mu57f4 <mustafataha874@gmail.com>
2024-05-22 13:56:14 +0300

    updated urls

--- job/urls.py
+++ job/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('create-job/', views.create_job, name='create-job'),
     path('update-job/<int:pk>/', views.update_job, name='update-job'),
+    path('manage-jobs/', views.manage_jobs, name='manage-jobs'),
+    path('job-details/<int:pk>', views.job_details, name='job-details'),
 ]
