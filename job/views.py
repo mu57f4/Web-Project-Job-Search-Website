5875f9a6a83ee14585fbde92cf7bc0da60795782
mu57f4 <mustafataha874@gmail.com>
2024-05-22 13:53:14 +0300

    Return to manage-jobs

--- job/views.py
+++ job/views.py
@@ -14,7 +14,7 @@ def create_job(request):
                 var.company = request.user.company
                 var.save()
                 messages.success(request, 'New job has been created.')
-                return redirect('dashboard')
+                return redirect('manage-jobs')
             else:
                 messages.warning(request, 'Opps!, something went wrong')
                 return redirect('create-job')
@@ -35,15 +35,26 @@ def update_job(request, pk):
             form = UpdateJobForm(request.POST, instance=job)
             if form.is_valid():
                 form.save()
-                messages.success(request, 'Job info has been updated.')
-                return redirect('dashboard')
+                messages.success(request, 'Job info has been updated successfully.')
+                return redirect('manage-jobs')
             else:
                 messages.warning(request, 'Opps!, something went wrong')
         else:
             form = UpdateJobForm(instance=job)
             context = {'form': form}
-            return render(request, 'job/create_job.html', context)
+            return render(request, 'job/update_job.html', context)
     else:
         messages.warning(request, 'Permission Denied')
         return redirect('dashboard')
 
+
+def manage_jobs(request):
+    jobs = Job.objects.filter(user=request.user, company=request.user.company)
+    context = {'jobs': jobs}
+    return render(request, 'job/manage_jobs.html', context)
+
+
+def job_details(request, pk):
+    job = Job.objects.get(pk=pk)
+    context = {'job': job}
+    return render(request, 'job/job_details.html', context)
