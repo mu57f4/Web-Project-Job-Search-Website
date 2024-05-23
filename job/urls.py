ec73d0b4acab1f129282c1956a97a64ebbd25594
mu57f4 <mustafataha874@gmail.com>
2024-05-23 09:36:46 +0300

    Added namespace and new urls

--- job/urls.py
+++ job/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'job' 
+
 urlpatterns = [
     path('create-job/', views.create_job, name='create-job'),
     path('update-job/<int:pk>/', views.update_job, name='update-job'),
     path('manage-jobs/', views.manage_jobs, name='manage-jobs'),
-    path('job-details/<int:pk>', views.job_details, name='job-details'),
+    path('<int:pk>/', views.job_details, name='job_detail'),
+    path('apply/<int:job_id>/', views.apply_for_job, name='apply_for_job'),
 ]
