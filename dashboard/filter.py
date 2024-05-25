018836ff780da358f34a3a1ab5cf806c81c3d4e4
mu57f4 <mustafataha874@gmail.com>
2024-05-25 14:21:08 +0300

    Updated search filter to YoE

--- dashboard/filter.py
+++ dashboard/filter.py
@@ -4,7 +4,6 @@ from job.models import Job
 
 class JobFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='icontains', label='Job Keyword')
-    location = django_filters.CharFilter(lookup_expr='icontains', label='Location')
     class Meta:
         model = Job
-        fields = ['title', 'location', 'job_type']
+        fields = ['title', 'years_of_experience', 'job_type']
